0bb6e46ca15a26bfc6ece612178ff0416a6cebb0
fazza2086 <132415607+fazza2086@users.noreply.github.com>
2023-05-17 10:59:41 +0700

    finish project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+<x-app-layout>
+    @section('content')
+    <div class="container">
+
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        {{ __('Dashboard') }}
+    </h2>
+
+
+<div class="py-12">
+    <p>ke halaman <a href="/siswa">siswa</a></p>
+</div>
+    </div>
+    @endsection
+    
+</x-app-layout>
